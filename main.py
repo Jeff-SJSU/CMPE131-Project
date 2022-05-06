7a14035e19546b23d435255971140f1737682198
Jeff-SJSU <jeffrey.ellison@sjsu.edu>
2022-05-05 17:14:15 -0700

    Cleanup old code

--- main.py
+++ main.py
@@ -10,22 +10,6 @@ from app.routes import *
 db.create_all()
 
 '''
-@app.route('/login/', methods=['GET', 'POST'])
-def login():
-    #   Method = POST
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-
-        if db_login(email, password) == True:
-            session['email'] = email
-            return redirect(url_for('home'))
-        else:
-            return redirect(url_for('home'))
-
-    #   Method = GET
-    return render_template('./login.html')
-
 @app.route('/logout')
 def logout():
     session['email'] = None
