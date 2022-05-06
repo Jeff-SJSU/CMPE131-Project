6ce9eede6aa6a276811bba48d14da6bd586d61dd
Jeff-SJSU <jeffrey.ellison@sjsu.edu>
2022-05-05 17:35:45 -0700

    Serve static files

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-app = Flask(__name__, template_folder='../templates')
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
 app.config.from_mapping(
     SECRET_KEY='super-secret',
     SQLALCHEMY_DATABASE_URI='sqlite:///' + os.path.join(basedir, 'database.db'),
