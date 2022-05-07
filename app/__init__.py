944b7d8473203325d79f50c019a58068e3235215
Jeff-SJSU <jeffrey.ellison@sjsu.edu>
2022-05-06 18:55:40 -0700

    Cleanup

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-print(basedir)
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
 app.config.from_mapping(
     SECRET_KEY='super-secret',
-    SQLALCHEMY_DATABASE_URI='sqlite:///' + os.path.join(basedir, 'databases.db'),
+    SQLALCHEMY_DATABASE_URI='sqlite:///' + os.path.join(basedir, 'database.db'),
     SQLALCHEMY_TRACK_MODIFICATIONS=False
 )
 
